e98a32e21dba455193cfbe982568dafa0b9e294e
Eric <fcweric@gmail.com>
2023-03-16 22:30:43 -0400

    Add login input to login

--- script.js
+++ script.js
@@ -5,16 +5,26 @@ const arrows = document.querySelectorAll(".arrow");
 const carousel = document.querySelector(".carousel-inner");
 const books = document.querySelectorAll(".book");
 
-// navbar and controls
-const navbar = document.getElementById("navBar");
 
-const navbarIcon = document.getElementById("navbarToggle");
+arrows[0].addEventListener("click", () => {
+    const dx = books[0].clientWidth;
+	carousel.scrollLeft -= dx;
+});
+
+arrows[1].addEventListener("click", () => {
+    const dx = books[0].clientWidth;
+	carousel.scrollLeft += dx;
+});
+
+for (const book of books) {
+    book.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+}
 
 // Open nav bar
 function toggleNavbar() {
+    const navbar = document.getElementById("navBar");
     try {
         changeNavIcon();
-        console.log(navbar.style.width);
         if (navbar.style.width.localeCompare("0px") == 0 || navbar.style.width.localeCompare("") == 0) {
             // Expand navbar
             navbar.style.width = "200px";
@@ -31,6 +41,7 @@ function toggleNavbar() {
 
 // Change nav bar icon
 function changeNavIcon() {
+    const navbarIcon = document.getElementById("navbarToggle");
     navbarIcon.classList.toggle("change"); // Change navbar icon
 }
 
@@ -53,19 +64,4 @@ function changeNavElementVis(login, username) {
         document.getElementById("welcome_msg").style.visibility = "collapse";
         document.getElementById("adminpage").style.visibility = "collapse"
     }
-}
-
-
-arrows[0].addEventListener("click", () => {
-	const dx = books[0].clientWidth;
-	carousel.scrollLeft -= dx;
-});
-
-arrows[1].addEventListener("click", () => {
-	const dx = books[0].clientWidth;
-	carousel.scrollLeft += dx;
-});
-
-for (const book of books) {
-	book.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
 }
